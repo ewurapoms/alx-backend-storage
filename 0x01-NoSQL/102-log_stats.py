68155eb2a72d25dbfea2724f0d451317d8ca4ba1
ewurapoms <mzpomaa35@gmail.com>
2024-02-27 02:46:43 -0800

    Advanced tasks

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+""" Module displays Nginx logs stored in MongoDB"""
+
+from pymongo import MongoClient
+
+if __name__ == "__main__":
+    client = MongoClient("localhost", 27017)
+    collection = client.logs.nginx
+
+    total_logs = collection.count_documents({})
+    print(f"{total_logs} logs")
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
+    for method in methods:
+        result = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {result}")
+
+    status_check_count = collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status_check_count} status check")
+
+    # gets the top 10 most present IPs
+    top_ips = collection.aggregate([
+        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}},
+        {"$limit": 10}
+    ])
+
+    print("Top 10 IPs:")
+    for ip in top_ips:
+        print(f"\tIP: {ip['_id']}, Count: {ip['count']}")
