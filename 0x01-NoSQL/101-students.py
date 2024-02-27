70dc3cf1971b926819969fac39f0948c944a13c2
ewurapoms <mzpomaa35@gmail.com>
2024-02-27 02:40:55 -0800

    modifying python script

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python3
 """ script that returns all students sorted by average score"""
 
-
 def top_students(mongo_collection):
     """ displays student scores"""
-    students = mongo_collection.find()
-    sorted_s = sorted(students, key=lambda student: student['averageScore'],
-                      reverse=True)
-    return sorted_s
+    return mongo_collection.aggregate([
+        {
+            "$project":
+                {
+                    "name": "$name",
+                    "averageScore": {"$avg": "$topics.score"}
+                }
+        },
+        {
+            "$sort":
+                {
+                    "averageScore": -1
+                }
+        }
+    ])
