a540ed0c84a82e3c881d3dc7998cf5310c41b84b
ewurapoms <mzpomaa35@gmail.com>
2024-02-27 03:07:12 -0800

    Modifying script for advanced task

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,19 +1,7 @@
 #!/usr/bin/env python3
-""" Module displays Nginx logs stored in MongoDB"""
+""" Module adds top 10 IPs to Nginx logs stored in MongoDB"""
 
 from pymongo import MongoClient
-
-
-def top_ips(collection):
-    """ displays the function """
-    pipeline = [
-        {"$group": {"_id": "$remote_addr", "count": {"$sum": 1}}},
-        {"$sort": {"count": -1}},
-        {"$limit": 10}
-    ]
-    top_ips = collection.aggregate(pipeline)
-    return list(top_ips)
-
 
 if __name__ == "__main__":
     client = MongoClient("localhost", 27017)
@@ -29,8 +17,18 @@ if __name__ == "__main__":
     status_check_count = collection.count_documents({"method": "GET",
                                                      "path": "/status"})
     print(f"{status_check_count} status check")
-
-    top_ips_list = top_ips(collection)
-    print("Top 10 IPs:")
-    for idx, ip_data in enumerate(top_ips_list, 1):
-        print(f"\t{idx}. IP: {ip_data['_id']}, Count: {ip_data['count']}")
+    top_ips = collection.aggregate([
+        {
+            "$group": {
+                "_id": "$ip",
+                "count": {
+                    "$sum": 1
+                }
+            }
+        },
+        {"$sort": {"count": -1}},
+        {"$limit": 10}
+    ])
+    print("IPs:")
+    for ip in top_ips:
+        print(f"\t{ip['_id']}: {ip['count']}")
