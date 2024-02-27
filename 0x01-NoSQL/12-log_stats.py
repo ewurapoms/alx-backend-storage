f34b8b3186724372ae469aadf62fd7aa6e8ec053
ewurapoms <mzpomaa35@gmail.com>
2024-02-27 02:27:07 -0800

    modifying script

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -3,17 +3,18 @@
 
 from pymongo import MongoClient
 
+
 if __name__ == "__main__":
     client = MongoClient("localhost", 27017)
     collection = client.logs.nginx
 
-    logged = collection.count_documents({})
-    print(f"Total logs: {logged}")
+    update = collection.count_documents({})
+    print(f"{update} logs")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
     for method in methods:
-        count = collection.count_documents({"method": method})
-        print(f"{method}: {count}")
-    count_status = collection.count_documents({"method": "GET",
-                                               "path": "/status"})
-
-    print(f"GET /status: {count_status}")
+        output = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {result}")
+    final_count = collection.count_documents({"method": "GET",
+                                              "path": "/status"})
+    print(f"{final_count} log check")
