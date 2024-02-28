f9af952c66a185d8a5ebd0b4d4af66e36b2e3e19
ewurapoms <mzpomaa35@gmail.com>
2024-02-28 09:06:33 -0800

    modifying script and fixing pep8 errors

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,34 +1,45 @@
 #!/usr/bin/env python3
 """Implementing an expiring web cache and tracker"""
 
-from functools import wraps
 import requests
-import redis
+import time
+from functools import wraps
 
-store = redis.Redis()
+cache = {}
 
 
-def count_url_access(method):
-    """ counts number of times a URL is accessed """
-    @wraps(method)
+def get_page(url: str) -> str:
+    """
+    checks if the URL is cached and not expired
+    and make the request and retrieve the HTML content
+    """
+    if url in cache and cache[url]['expiration'] > time.time():
+        cache[url]['count'] += 1
+        return cache[url]['content']
+    response = requests.get(url)
+    con = response.text
+    cache[url] = {'content': con, 'expiration': time.time() + 10, 'count': 1}
+    return con
+
+
+def cache_result(func):
+    """
+    Decorator to track URL access count
+    and cache the result
+    """
+    @wraps(func)
     def wrapper(url):
-        cached_key = "cached:" + url
-        cached_data = store.get(cached_key)
-        if cached_data:
-            return cached_data.decode("utf-8")
-
-        count_key = "count:" + url
-        html = method(url)
-
-        store.incr(count_key)
-        store.set(cached_key, html)
-        store.expire(cached_key, 10)
-        return html
+        if url in cac and cac[url]['expiration'] > time.time():
+            cac[url]['count'] += 1
+            return cac[url]['content']
+        con = func(url)
+        cac[url] = {'content': con, 'expiration': time.time() + 10, 'count': 1}
+        return con
     return wrapper
 
 
-@count_url_access
-def get_page(url: str) -> str:
-    """ prints HTML content of a url """
-    res = requests.get(url)
-    return res.text
+@cache_result
+def get_page_with_decorator(url: str) -> str:
+    """ prints the output"""
+    response = requests.get(url)
+    return response.text
