85fcb5e4de904104d2a473b01d4bd822e21469e5
ewurapoms <mzpomaa35@gmail.com>
2024-02-28 10:01:45 -0800

    still debugging

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -5,52 +5,41 @@ import requests
 import time
 from functools import wraps
 
-# Dictionary to store cached pages and access counts
 cache = {}
 
 
-def cache_with_expiry(expiry=10):
-    def decorator(func):
-        @wraps(func)
-        def wrapper(url):
-            if url in cache:
-                # Check if the cached entry is still valid (within expiry time)
-                if time.time() - cache[url]['timestamp'] < expiry:
-                    cache[url]['count'] += 1
-                    return cache[url]['content']
-
-            # If not cached or expired, fetch the page content
-            content = func(url)
-
-            # Update cache with new content and reset access count
-            cache[url] = {
-                'content': content,
-                'count': 1,
-                'timestamp': time.time()
-            }
-            return content
-
-        return wrapper
-
-    return decorator
-
-
-@cache_with_expiry(expiry=10)
 def get_page(url: str) -> str:
     """
-    Retrieve the HTML content of a URL.
-
-    The function uses the requests module to obtain
-    the HTML content of the given URL.
-    It also tracks the number of times the URL was
-    accessed and caches the result with
-    an expiration time of 10 seconds.
-
-    Args:
-        url (str): The URL to retrieve the HTML content from.
-
-    Returns:
-        str: The HTML content of the URL.
+    checks if the URL is cached and not expired
+    and make the request and retrieve the HTML content
     """
+    if url in cache and cache[url]['expiration'] > time.time():
+        cache[url]['count'] += 1
+        return cache[url]['content']
+    response = requests.get(url)
+    con = response.text
+    cache[url] = {'content': con, 'expiration': time.time() + 10, 'count': 1}
+    return con
+
+
+def cache_result(func):
+    """
+    Decorator to track URL access count
+    and cache the result
+    """
+    @wraps(func)
+    def wrapper(url):
+        if url in cac and cac[url]['expiration'] > time.time():
+            cac[url]['count'] += 1
+            return cac[url]['content']
+        con = func(url)
+        cac[url] = {'content': con, 'expiration': time.time() + 10, 'count': 1}
+        return con
+    return wrapper
+
+
+@cache_result
+def get_page_with_decorator(url: str) -> str:
+    """ prints the output"""
     response = requests.get(url)
     return response.text
