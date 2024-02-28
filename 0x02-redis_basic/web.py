0a04062b4edf8c1f02b2459720a5ef041928a024
ewurapoms <mzpomaa35@gmail.com>
2024-02-28 08:37:57 -0800

    modifying script

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -8,27 +8,29 @@ import redis
 store = redis.Redis()
 
 
-def count_url_access(func):
+def count_url_access(method):
     """ counts number of times a URL is accessed """
-    @wraps(func)
+    @wraps(method)
     def wrapper(url):
-        cached_key = f"cached:{url}"
+        cached_key = "cached:" + url
         cached_data = store.get(cached_key)
         if cached_data:
             return cached_data.decode("utf-8")
 
-        update = f"count:{url}"
-        html = func(url)
+        count_key = "count:" + url
+        html = method(url)
 
-        store.incr(update)
+        store.incr(count_key)
         store.set(cached_key, html)
         store.expire(cached_key, 10)
         return html
     return wrapper
 
 
-@count_url_access
 def get_page(url: str) -> str:
     """ prints HTML content of a url """
     res = requests.get(url)
     return res.text
+
+
+get_page = count_url_access(get_page)
