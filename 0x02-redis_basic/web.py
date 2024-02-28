04e0b08c334d8c277c8ea57b714584be6796fe3c
ewurapoms <mzpomaa35@gmail.com>
2024-02-28 09:32:30 -0800

    testing

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,45 +1,36 @@
 #!/usr/bin/env python3
-"""Implementing an expiring web cache and tracker"""
-
+"""
+web cache and tracker
+"""
 import requests
-import time
+import redis
 from functools import wraps
 
-cache = {}
+store = redis.Redis()
 
 
-def get_page(url: str) -> str:
-    """
-    checks if the URL is cached and not expired
-    and make the request and retrieve the HTML content
-    """
-    if url in cache and cache[url]['expiration'] > time.time():
-        cache[url]['count'] += 1
-        return cache[url]['content']
-    response = requests.get(url)
-    con = response.text
-    cache[url] = {'content': con, 'expiration': time.time() + 10, 'count': 1}
-    return con
-
-
-def cache_result(func):
-    """
-    Decorator to track URL access count
-    and cache the result
-    """
-    @wraps(func)
+def count_url_access(method):
+    """ Decorator counting how many times
+    a URL is accessed """
+    @wraps(method)
     def wrapper(url):
-        if url in cac and cac[url]['expiration'] > time.time():
-            cac[url]['count'] += 1
-            return cac[url]['content']
-        con = func(url)
-        cac[url] = {'content': con, 'expiration': time.time() + 10, 'count': 1}
-        return con
+        cached_key = "cached:" + url
+        cached_data = store.get(cached_key)
+        if cached_data:
+            return cached_data.decode("utf-8")
+
+        count_key = "count:" + url
+        html = method(url)
+
+        store.incr(count_key)
+        store.set(cached_key, html)
+        store.expire(cached_key, 10)
+        return html
     return wrapper
 
 
-@cache_result
-def get_page_with_decorator(url: str) -> str:
-    """ prints the output"""
-    response = requests.get(url)
-    return response.text
+@count_url_access
+def get_page(url: str) -> str:
+    """ Returns HTML content of a url """
+    res = requests.get(url)
+    return res.text
